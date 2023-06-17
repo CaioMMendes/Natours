26754194712b37dc9baa353ded1d5ac32b515745
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2023-06-17 12:42:30 -0300

    start with mongoose

--- controllers/tourController.ts
+++ controllers/tourController.ts
@@ -1,64 +1,96 @@
 import fs from 'fs';
 import { NextFunction, Request, Response } from 'express';
+import Tour from '../models/tourModel';
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'utf-8')
-);
-export const checkId = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-  val: string
-) => {
-  console.log('o id é:', val);
-  if (!req.params.id) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id',
-    });
-  }
+// export const checkId = (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction,
+//   val: string
+// ) => {
+//   console.log('o id é:', val);
+//   if (!req.params.id) {
+//     return res.status(404).json({
+//       status: 'fail',
+//       message: 'Invalid Id',
+//     });
+//   }
 
-  next();
-};
-
-export const checkBody = (req: Request, res: Response, next: NextFunction) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Invalid body',
-    });
-  }
-  next();
-};
+//   next();
+// };
 
 interface RequestWithTime extends Request {
   requestTime: string;
 }
-export const getAllTours = (req: Request, res: Response) => {
-  res.status(200).json({
-    status: 'sucess',
-    requestTime: (req as RequestWithTime).requestTime,
-    data: { tours },
-  });
+export const getAllTours = async (req: Request, res: Response) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'sucess',
+      // requestTime: (req as RequestWithTime).requestTime,
+      data: { tours },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'error',
+
+      message: error,
+    });
+  }
 };
-export const getTour = (req: Request, res: Response) => {
-  console.log(req.params);
-  res.status(200).json({
-    status: 'sucess',
-    id: req.params.id,
-    data: { tours },
-  });
+export const getTour = async (req: Request, res: Response) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'sucess',
+      id: req.params.id,
+      data: { tour },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'error',
+      message: error,
+    });
+  }
 };
-export const createTour = (req: Request, res: Response) => {
-  res.send(req.body);
+export const createTour = async (req: Request, res: Response) => {
+  try {
+    const tour = await Tour.create(req.body);
+    res.status(201).send({
+      status: 'sucess',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      status: 'error',
+      message: error,
+    });
+  }
 };
-export const updateTour = (req: Request, res: Response) => {
-  const string = req.params.id;
-  const number = +'2';
-  console.log(3 > +'2');
-  console.log(typeof string);
-  console.log(typeof number);
-  res.send('ok');
+export const updateTour = async (req: Request, res: Response) => {
+  try {
+    const updatedTour = await Tour.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.status(201).send({
+      status: 'sucess',
+      data: {
+        updatedTour,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      status: 'error',
+      message: error,
+    });
+  }
 };
 
 export const deleteTour = (req: Request, res: Response) => {
--- server.ts
+++ server.ts
@@ -2,6 +2,32 @@
 // dotenv.config({ path: './config.env' });
 
 import { app } from './app';
+import mongoose from 'mongoose';
+
+let DB: string;
+
+if (process.env.DATABASE && process.env.DATABASE_PASSWORD) {
+  DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+  );
+} else {
+  throw new Error('Set the environment variables');
+}
+
+main().catch((err) => console.log(err));
+async function main() {
+  // if (process.env.DATABASE_LOCAL) {
+  //   await mongoose.connect(process.env.DATABASE_LOCAL).then((con) => {
+  //     // console.log(con.connections);
+  //     console.log('DB connected');
+  //   });
+  // }
+  await mongoose.connect(DB).then((con) => {
+    // console.log(con.connections);
+    console.log('DB connected');
+  });
+}
 
 app.listen(process.env.PORT, () => {
   console.log(`Running on port ${process.env.PORT}`);
