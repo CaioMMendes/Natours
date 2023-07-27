209c766502cc1382dc1098bbd4fcd757e6a1cba7
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2023-07-27 20:09:04 -0300

    feat: add user model validation

--- models/userModel.ts
+++ models/userModel.ts
@@ -1,14 +1,22 @@
 import mongoose from 'mongoose';
+import validator from 'validator';
+import isEmail from 'validator/lib/isEmail';
+import bcrypt from 'bcryptjs';
+import { Document } from 'mongoose';
 
-interface User {
+export interface IUser {
   name: string;
   email: string;
   photo: string;
   password: string;
-  passwordConfirm: string;
+  passwordConfirm: string | undefined;
+  correctPassword(
+    candidatePassword: string,
+    userPassword: string
+  ): Promise<boolean>;
 }
 
-const userSchema = new mongoose.Schema<User>({
+const userSchema = new mongoose.Schema<IUser>({
   name: {
     type: String,
     required: [true, 'A User must have a name'],
@@ -18,7 +26,9 @@ const userSchema = new mongoose.Schema<User>({
     type: String,
     required: [true, 'A User must have a e-mail'],
     trim: true,
+    lowercase: true,
     unique: true,
+    validate: [validator.isEmail, 'You must pass a valid e-mail'],
   },
   photo: {
     type: String,
@@ -27,10 +37,39 @@ const userSchema = new mongoose.Schema<User>({
     type: String,
     required: [true, 'A User must have a password'],
     trim: true,
+    minlength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
     required: [true, 'A User must have a passwordConfirm'],
     trim: true,
+    validate: {
+      //this only work on save
+      validator: function (this: IUser, el: string): boolean {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
+    select: false,
   },
 });
+
+userSchema.pre('save', async function (next) {
+  //only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword: string,
+  userPassword: string
+): Promise<boolean> {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
